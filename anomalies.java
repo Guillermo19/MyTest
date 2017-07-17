cc65372e727659a90fe8f2f3fecf596f496e0db8
Guillermo Herrera <gherreramelara@troy.edu>
2017-07-17 01:21:12 -0400

    Update anomalies.java

--- anomalies.java
+++ anomalies.java
@@ -1,4 +1,7 @@
+package org.apache.anomaly.detection;
 import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -16,7 +19,7 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 
-public class anomalies {
+public class Anomalies {
 	private LinearRegression linReg;
 	private LinearRegressionModel model;
 	
@@ -26,7 +29,7 @@ public class anomalies {
 	
 	private ArrayList<Row> anomalies;
 	
-	private final static double threshold = 2.0;
+	private final static double threshold = 3.0;
 	
 	private final static SparkConf sparkConf = 
 			new SparkConf()
@@ -41,7 +44,7 @@ public class anomalies {
 			  .appName("Anomaly Detection")
 			  .getOrCreate();
 	
-	public anomalies(String filePath) {
+	public Anomalies(String filePath) {
 	
 		dataset = sparkSession.read().
 				option("header", "true").
@@ -56,26 +59,52 @@ public class anomalies {
 				.setOutputCol("features");
 		
 		vectorDataset = assembler.transform(dataset); //converts and combines the 'size' and 'records' column as the vector type 'features' column
+		vectorDataset.cache();
 		
-		linReg = new LinearRegression().setLabelCol("Volume");
+		linReg = new LinearRegression().setLabelCol("Volume").setStandardization(true);
 		
 		model = linReg.fit(vectorDataset); //trains the k-means with the dataset to create a model
+		model.summary().residuals().show();
+		anomalies = new ArrayList<>();
 		
 	} //end constructor
 	
-	private void areThereAnomalies(){
+	private boolean areThereAnomalies(){
+		boolean areAnomalies = false;
 		residuals = model.summary().residuals();
+		Column residualCol = new Column("residuals");
 		double residualStddev = sparkContext.parallelizeDoubles(residuals.toJavaRDD().map(f -> f.getDouble(0)).collect()).stdev();
+		Dataset<Row> standardResiduals = residuals.withColumn("standardResiduals", residualCol.divide(residualStddev)).drop(residualCol);
 		
+		List<Row> stdResidList = standardResiduals.collectAsList();
+		List<Row> list = vectorDataset.collectAsList();
+		
+		standardResiduals.show();
+		for(int i = 0; i<stdResidList.size(); i++){
+			double residual = stdResidList.get(i).getDouble(0);
+			
+			if(Math.abs(residual) > threshold){
+				anomalies.add(list.get(i));
+				areAnomalies = true;
+				vectorDataset = vectorDataset.filter(new Column("File").notEqual(list.get(i).getAs("File")));
+			}
+		}
+		
+		return areAnomalies;
 	}
 	private void getAnomalies(){ //identifies potential outliers that might be mixed with normal data and saves them
+		boolean anomalies = this.areThereAnomalies();
+		while(anomalies){
+			model = linReg.fit(vectorDataset);
+			anomalies = this.areThereAnomalies();
+		}
 		
 	}//end get anomalies
 	
 	public void printAnomalies() {
-		this.areThereAnomalies();
+		this.getAnomalies();
 		//vectorDataset.show(25,false);
-		/*if(!anomalies.isEmpty()){
+		if(!anomalies.isEmpty()){
 			System.out.println("Possible anomalous files: ");
 			for(Row r : anomalies){
 				System.out.println(r.getAs("File").toString());
@@ -83,7 +112,7 @@ public class anomalies {
 		}
 		else{
 			System.out.println("No probable anomalous files");
-		}*/
+		}
 		
 	} //end print anomalies 
 }
