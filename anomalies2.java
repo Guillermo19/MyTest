30d95c60c7b6c3ed40b17fd0e90a5a1ebf405b64
Guillermo Herrera <gherreramelara@troy.edu>
2017-08-01 11:06:13 -0400

    Update anomalies2.java

--- anomalies2.java
+++ anomalies2.java
@@ -1,4 +1,6 @@
 import java.util.List;
+import java.util.Scanner;
+import java.io.File;
 import java.util.ArrayList;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -11,7 +13,6 @@ import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.functions;
 
 public class DetectAnomalousData {
 	
@@ -30,26 +31,48 @@ public class DetectAnomalousData {
 	
 	private Dataset<Row> training;
 	private Dataset<Row> test;
+	private Dataset<Row> unlabeledData;
 	
-	private final Column anomalous = new Column("anomalous");
+	private String trainingFile;
+	private String testFile;
 	private String[] columnNames;
 	
 	private GBTClassifier classifier;
 	private GBTClassificationModel model;
 	
-	public DetectAnomalousData(String goodFile, String badFile) {
-	
+	public DetectAnomalousData() {
+		File file;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the name of the file you want to train the model with: ");
+		trainingFile = in.nextLine();
+		file = new File(trainingFile);
+		while(!file.exists()){
+			System.out.println("Enter the name of the file you want to train the model with: ");
+			trainingFile = in.nextLine();
+			file = new File(trainingFile);
+		}
+		
+		System.out.println("Enter the name of the file you want to check for possible erros: ");
+		testFile = in.nextLine();
+		file = new File(testFile);
+		while(!file.exists()){
+			System.out.println("Enter the name of the file you want to train the model with: ");
+			testFile = in.nextLine();
+			file = new File(testFile);
+		}
+		in.close();
+		
 		training = sparkSession.read()
 				  .option("header", "true")
 				  .option("inferSchema", "true")
 				  .option("delimiter", "\t")
-				  .csv(goodFile);
+				  .csv(trainingFile);
 		
 		test = sparkSession.read()
 				  .option("header", "true")
 				  .option("inferSchema", "true")
 				  .option("delimiter", "\t")
-				  .csv(badFile);
+				  .csv(testFile);
 		
 		training.cache();
 		test.cache();
@@ -58,25 +81,34 @@ public class DetectAnomalousData {
 	
 	//prepares the dataset for training on the specified columns 
 	private void prepareDataset(){
-		VectorAssembler assembler;
+		VectorAssembler featureAssembler;
+		List<String> featureCols = new ArrayList<>();
+		StringIndexerModel indexer;
+		Dataset<Row> temp = test;
 		
-		for(String col : training.columns()) {
-			if(training.head().getAs(col).getClass().getSimpleName().equals("String")){
-				List<Row> stringValues = training.select(col).distinct().collectAsList();
+		for(String column : columnNames){
+			if(training.head().getAs(column).getClass().getSimpleName().equals("String")){
+				Column col = new Column(column);
 				
-				for(Row row : stringValues){
-					Object val = row.get(0);
-					training = training.withColumn("HASH"+col, functions.when(new Column(col).equalTo(val).and(other), val.hashCode()));
-				}
+				indexer = new StringIndexer().setInputCol(column).setOutputCol("INDEX"+column).fit(training);
+				training = indexer.transform(training);
+				
+				List<String> knownValues = training.select(col).toJavaRDD().map(f -> f.getString(0)).collect();
+				this.checkUnknownData(temp, column, knownValues);
+				test = indexer.transform(test.filter(col.isin(knownValues.toArray())));
+				
+				featureCols.add("INDEX"+column);
 			}
+			else
+				featureCols.add(column);
 		}
-		
-		
-		assembler = new VectorAssembler().setInputCols(training.columns()).setOutputCol("features");
-		
-		training = assembler.transform(training);
-		test = assembler.transform(test);
 
+		String[] features = featureCols.toArray(new String[0]);
+		featureAssembler = new VectorAssembler().setInputCols(features).setOutputCol("features");
+		
+		training = featureAssembler.transform(training);
+		test = featureAssembler.transform(test);
+		
 	}
 	
 	//checks for anomalous data training the model on the whole dataset
@@ -84,13 +116,26 @@ public class DetectAnomalousData {
 		this.prepareDataset();
 		classifier = new GBTClassifier().setLabelCol("anomalous");
 		model = classifier.fit(training);
-
 	}
 	
+	//displaus the possible anomalies to the user  
 	public void printAnomalies(){
 		this.checkForAnomalies();
 		Column anomaly = new Column("prediction");
 		Dataset<Row> anomalies = model.transform(test).filter(anomaly.equalTo(1));
-		anomalies.select("prediction", columnNames).show();
+		System.out.println("Possible anomalous data: ");
+		anomalies.select("prediction", columnNames).drop("prediction").show();
+		//System.out.println(model.toDebugString());
+	}
+	
+	//checks for categorical values NOT present on the training dataset and removes them from the testing set
+	private void checkUnknownData(Dataset<Row> temp, String column, List<String> knownValues){
+		Column col = new Column(column);
+		Column condition = temp.col(column).equalTo(temp.filter(col.isin(knownValues.toArray())).col(column));
+		unlabeledData = temp.join(temp.filter(col.isin(knownValues.toArray())), condition, "leftanti");
+		if(unlabeledData.count() > 0){
+			System.out.println("\nUnknown value(s) in: " + testFile);
+			unlabeledData.select(col).show();
+		}
 	}
 }
